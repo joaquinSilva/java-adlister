6a18c8f07ed8a41316a97bb4fb9887472a52a392
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-12-07 15:43:57 -0600

    completed hellow world servlet exercise and created and completed page view counter exercise.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        if (name == null) {
+            out.format("<h1>Hello, World!</h1>");
+        } else {
+            out.write("<h1>Hello, " + name + "!</h1>");
+        }
     }
 }
+
---
+++ src/main/java/PageViewCounter.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "PageViewCounter", urlPatterns = "/count")
+public class PageViewCounter extends HttpServlet {
+
+    public int counter = 0;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        counter+=1;
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String reset = req.getParameter("resetCounter");
+
+
+        if (reset == null) {
+            out.format("<h1>Number of times this page has been viewed: " + Integer.toString(counter) + " </h1>");
+        } else {
+            counter = 1;
+            out.format("<h1>Number of times this page has been viewed: " + Integer.toString(counter) + " </h1>");
+        }
+
+    }
+}
+
